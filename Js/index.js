1e8a33af760f9473e4020ef8e01f7e459dd9b5da
AK <akashkalpande2020@gmail.com>
2021-11-14 12:39:46 +0530

    initial commit

---
+++ Js/index.js
@@ -0,0 +1,10 @@
+const a = document.getElementById("b");
+function date() {
+    a.innerHTML = console.log(Date());
+}
+
+function myClick(){
+    document.getElementById("aa").style.color = "red";
+    document.getElementById('aa').style.backgroundImage="/imgg/a1jfif.jpg"
+}
+
